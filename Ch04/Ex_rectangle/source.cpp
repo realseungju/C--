9a215c789df55c0caf4cadbd604fcdce6df37831
realseungju <92769406+realseungju@users.noreply.github.com>
2022-03-23 11:51:47 +0900

    Practice
    Ch04 Examples

---
+++ Ch04/Ex_rectangle/source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    int width;
+    int height;
+    int getArea();
+};
+
+int Rectangle::getArea() {
+  return width*height;
+}
+
+int main()
+{
+    Rectangle rect;
+    int wid;
+    int hei;
+
+    cout << "사각형 가로의 길이>> ";
+    cin >> wid;
+    cout << "사각형 세로의 길이>> ";
+    cin >> hei;
+
+    rect.width = wid;
+    rect.height = hei;
+    
+    cout << "사각형의 면적은 " << rect.getArea() << endl;
+}
